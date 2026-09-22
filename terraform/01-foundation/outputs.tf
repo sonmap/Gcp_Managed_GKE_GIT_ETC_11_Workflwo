9175319cce1fb8fc04c2270a548d239c3c416d55
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-09-22 13:04:40 +0900

    Add foundation outputs

---
+++ terraform/01-foundation/outputs.tf
@@ -0,0 +1,31 @@
+output "project_id" {
+  value = var.project_id
+}
+
+output "region" {
+  value = var.region
+}
+
+output "source_bucket" {
+  value = google_storage_bucket.source.name
+}
+
+output "target_bucket" {
+  value = google_storage_bucket.target.name
+}
+
+output "artifact_repository" {
+  value = google_artifact_registry_repository.app.repository_id
+}
+
+output "artifact_image_base" {
+  value = "${var.region}-docker.pkg.dev/${var.project_id}/${google_artifact_registry_repository.app.repository_id}/gcs-transfer"
+}
+
+output "run_service_account" {
+  value = google_service_account.run.email
+}
+
+output "workflow_service_account" {
+  value = google_service_account.workflow.email
+}
